e39a2076800472d06aea4b9a5eb57353fb11aee6
Slobodan Zivanovic <198983353+slobodanzivanovic@users.noreply.github.com>
2025-02-16 00:21:24 +0100

    Auth service - Enums for User status/type

---
+++ backend/auth-service/src/main/java/com/slobodanzivanovic/dpmsn/authservice/model/auth/enums/UserStatus.java
@@ -0,0 +1,9 @@
+package com.slobodanzivanovic.dpmsn.authservice.model.auth.enums;
+
+/**
+ * Represents the status of a user
+ * This enum defines the different states a user can have in the system
+ */
+public enum UserStatus {
+	ACTIVE, PASSIVE, SUSPENDED
+}
---
+++ backend/auth-service/src/main/java/com/slobodanzivanovic/dpmsn/authservice/model/auth/enums/UserType.java
@@ -0,0 +1,9 @@
+package com.slobodanzivanovic.dpmsn.authservice.model.auth.enums;
+
+/**
+ * Represents the type of a user in the system
+ * This enum defines the different roles a user can have
+ */
+public enum UserType {
+	USER, ADMIN
+}
