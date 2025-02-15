fae0dc9d18911cfbf276d43880d577531f9dd9e2
Slobodan Zivanovic <198983353+slobodanzivanovic@users.noreply.github.com>
2025-02-16 00:06:57 +0100

    Auth service - BaseMapper, doc comments...

--- backend/auth-service/src/main/java/com/slobodanzivanovic/dpmsn/authservice/model/auth/Token.java
+++ backend/auth-service/src/main/java/com/slobodanzivanovic/dpmsn/authservice/model/auth/Token.java
@@ -5,6 +5,10 @@ import org.springframework.util.StringUtils;
 import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * Represents an authentication token named {@link Token} used for securing API requests
+ * This class contains the access token, its expiration time, and the refresh token
+ */
 @Getter
 @Builder
 public class Token {
@@ -15,11 +19,24 @@ public class Token {
 
 	private static final String TOKEN_PREFIX = "Bearer ";
 
+	/**
+	 * Checks if the given authorization header contains a Bearer token
+	 *
+	 * @param authorizationHeader the Authorization header value
+	 * @return {@code true} if the header starts with "Bearer "; {@code false} otherwise
+	 */
 	public static boolean isBearerToken(final String authorizationHeader) {
 		return StringUtils.hasText(authorizationHeader) &&
 				authorizationHeader.startsWith(TOKEN_PREFIX);
 	}
 
+	/**
+	 * Extracts the JWT from the given Authorization header
+	 * The JWT is expected to follow the Bearer token prefix.
+	 *
+	 * @param authorizationHeader the Authorization header value
+	 * @return the JWT extracted from the header, without the "Bearer " prefix
+	 */
 	public static String getJwt(final String authorizationHeader) {
 		return authorizationHeader.replace(TOKEN_PREFIX, "");
 	}
