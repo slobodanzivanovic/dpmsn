8d7cf0148d5c2fa3b4737bf1bd72329a296d4c84
Slobodan Zivanovic <198983353+slobodanzivanovic@users.noreply.github.com>
2025-02-16 02:24:49 +0100

    User service - User and token model

---
+++ backend/user-service/src/main/java/com/slobodanzivanovic/dpmsn/userservice/model/user/User.java
@@ -0,0 +1,38 @@
+package com.slobodanzivanovic.dpmsn.userservice.model.user;
+
+import com.slobodanzivanovic.dpmsn.userservice.model.common.BaseDomainModel;
+import com.slobodanzivanovic.dpmsn.userservice.model.user.enums.UserStatus;
+import com.slobodanzivanovic.dpmsn.userservice.model.user.enums.UserType;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * Represents a user domain object named {@link User} in the system
+ * The {@code User} class is a domain model that contains user-related information such as
+ * identification, contact details, status, type, and password. It extends {@link BaseDomainModel}
+ */
+@Getter
+@Setter
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class User extends BaseDomainModel {
+
+    private String id;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+    private UserStatus userStatus;
+
+    private UserType userType;
+
+    private String password;
+
+}
