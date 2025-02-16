f6f824854906cd164fce63ddcd157aab4c03d489
Slobodan Zivanovic <198983353+slobodanzivanovic@users.noreply.github.com>
2025-02-16 13:14:38 +0100

    User service - Security, config, filter

---
+++ backend/user-service/src/main/java/com/slobodanzivanovic/dpmsn/userservice/service/impl/InvalidTokenServiceImpl.java
@@ -0,0 +1,57 @@
+package com.slobodanzivanovic.dpmsn.userservice.service.impl;
+
+import com.slobodanzivanovic.dpmsn.userservice.exception.TokenAlreadyInvalidatedException;
+import com.slobodanzivanovic.dpmsn.userservice.model.user.entity.InvalidTokenEntity;
+import com.slobodanzivanovic.dpmsn.userservice.repository.InvalidTokenRepository;
+import com.slobodanzivanovic.dpmsn.userservice.service.InvalidTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Implementation of {@link InvalidTokenService} for managing invalid tokens
+ */
+@Service
+@RequiredArgsConstructor
+public class InvalidTokenServiceImpl implements InvalidTokenService {
+
+	private final InvalidTokenRepository invalidTokenRepository;
+
+	/**
+	 * Invalidates a set of tokens by saving them as invalid in the repository
+	 *
+	 * @param tokenIds a set of token IDs to be invalidated
+	 */
+	@Override
+	public void invalidateTokens(Set<String> tokenIds) {
+
+		final Set<InvalidTokenEntity> enocaInvalidTokenEntities = tokenIds.stream()
+			.map(tokenId -> InvalidTokenEntity.builder()
+				.tokenId(tokenId)
+				.build()
+			)
+			.collect(Collectors.toSet());
+
+		invalidTokenRepository.saveAll(enocaInvalidTokenEntities);
+	}
+
+	/**
+	 * Checks if a token has been invalidated by its ID
+	 *
+	 * @param tokenId the token ID to check
+	 * @throws TokenAlreadyInvalidatedException if the token has already been invalidated
+	 */
+	@Override
+	public void checkForInvalidityOfToken(String tokenId) {
+
+		final boolean isTokenInvalid = invalidTokenRepository.findByTokenId(tokenId).isPresent();
+
+		if (isTokenInvalid) {
+			throw new TokenAlreadyInvalidatedException(tokenId);
+		}
+
+	}
+
+}
